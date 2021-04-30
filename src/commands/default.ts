f77c6f71645d175e2ccb1948483ae7de47773402
Sergio Gómez Cosgaya <sergiogcosgaya@gmail.com>
2021-04-30 21:40:01 +0200

    Fixed git, yarn and npm commands returning a 0 exit code even the command failed

--- src/commands/default.ts
+++ src/commands/default.ts
@@ -5,7 +5,7 @@ import { catchError } from 'lib/error';
 import log from 'lib/log';
 import { GIT_COMMANDS } from 'lib/git';
 import { getCurrentPackageJson, getNodePackageManager, NPM_COMMANDS, YARN_COMMANDS } from 'lib/node';
-import { exec } from 'lib/shell';
+import { exec, execWithThrow } from 'lib/shell';
 
 const handler = (args: string[]) => {
 	catchError(() => {
@@ -13,7 +13,7 @@ const handler = (args: string[]) => {
 			const command = args[0];
 			const fullCommand = args.join(' ');
 
-			if (GIT_COMMANDS.includes(command)) return exec(`git ${fullCommand}`);
+			if (GIT_COMMANDS.includes(command)) return execWithThrow(`git ${fullCommand}`);
 
 			const pkgManager = getNodePackageManager();
 			let pkgJson;
@@ -21,11 +21,13 @@ const handler = (args: string[]) => {
 				pkgJson = getCurrentPackageJson();
 			} catch (e) {} // eslint-disable-line no-empty
 			if (pkgManager === 'yarn') {
-				if (YARN_COMMANDS.includes(command)) return exec(`yarn ${fullCommand}`);
-				if (pkgJson && (pkgJson.scripts as Record<string, unknown>)[command]) return exec(`yarn ${fullCommand}`);
+				if (YARN_COMMANDS.includes(command)) return execWithThrow(`yarn ${fullCommand}`);
+				if (pkgJson && (pkgJson.scripts as Record<string, unknown>)[command])
+					return execWithThrow(`yarn ${fullCommand}`);
 			} else if (pkgManager === 'npm') {
-				if (NPM_COMMANDS.includes(command)) return exec(`npm ${fullCommand}`);
-				if (pkgJson && (pkgJson.scripts as Record<string, unknown>)[command]) return exec(`npm run ${fullCommand}`);
+				if (NPM_COMMANDS.includes(command)) return execWithThrow(`npm ${fullCommand}`);
+				if (pkgJson && (pkgJson.scripts as Record<string, unknown>)[command])
+					return execWithThrow(`npm run ${fullCommand}`);
 			}
 
 			throw { message: `The command ${command} does not exist.` };
