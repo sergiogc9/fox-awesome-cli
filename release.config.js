7619c1251de73aeedc237d157f89729a4134367b
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2022-11-27 16:23:49 +0100

    chore: Specify main branch as a release branch in semantic-release config

--- release.config.js
+++ release.config.js
@@ -1,4 +1,5 @@
 const config = {
+	branches: ['main', { name: 'beta', prerelease: true }, { name: 'alpha', prerelease: true }],
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
